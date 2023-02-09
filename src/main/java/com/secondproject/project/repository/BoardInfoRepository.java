a9cba78f6a0d990791dc9160643831859d6528bd
jinhee <bis04199@gmail.com>
2023-02-09 15:43:59 +0900

    [박진희] entity

---
+++ src/main/java/com/secondproject/project/repository/BoardInfoRepository.java
@@ -0,0 +1,9 @@
+package com.secondproject.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.secondproject.project.entity.BoardInfoEntity;
+
+public interface BoardInfoRepository extends JpaRepository<BoardInfoEntity, Long>{
+    
+}
