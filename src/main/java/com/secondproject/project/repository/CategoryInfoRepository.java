cf86ee440d6cb5149e6ec0b26a8831f45f0ba033
yuje002 <bbongeva505@gmail.com>
2023-02-10 15:34:12 +0900

    [이민석]1

--- src/main/java/com/secondproject/project/repository/CategoryInfoRepository.java
+++ src/main/java/com/secondproject/project/repository/CategoryInfoRepository.java
@@ -1,11 +1,9 @@
 package com.secondproject.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.secondproject.project.entity.CategoryInfoEntity;
 
-@Repository
 public interface CategoryInfoRepository extends JpaRepository<CategoryInfoEntity, Long>{
     
 }
--- src/main/java/com/secondproject/project/repository/CommentInfoRepository.java
+++ src/main/java/com/secondproject/project/repository/CommentInfoRepository.java
@@ -1,11 +1,9 @@
 package com.secondproject.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.secondproject.project.entity.CommentInfoEntity;
 
-@Repository
 public interface CommentInfoRepository extends JpaRepository<CommentInfoEntity, Long>{
     
 }
