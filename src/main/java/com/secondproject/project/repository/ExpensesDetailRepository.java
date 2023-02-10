b183f77422b209b937a495262907a2dfc5d1af48
jinhee <bis04199@gmail.com>
2023-02-10 15:25:15 +0900

    [박진희]test

--- src/main/java/com/secondproject/project/repository/ExpensesDetailRepository.java
+++ src/main/java/com/secondproject/project/repository/ExpensesDetailRepository.java
@@ -1,9 +1,13 @@
 package com.secondproject.project.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.secondproject.project.entity.ExpensesDetailEntity;
+import com.secondproject.project.entity.MemberInfoEntity;
 
 public interface ExpensesDetailRepository extends JpaRepository<ExpensesDetailEntity, Long>{
-    
+    List<ExpensesDetailEntity> findByEdMiSeqAndEdDateBetween(MemberInfoEntity member, LocalDate start, LocalDate end);
 }
