be50fb9be8e545223746ed0b9d077a4f88d2cf0b
minseok <jhjhg010@gmail.com>
2023-02-14 15:57:41 +0900

    이민석1

--- src/main/java/com/secondproject/project/repository/MemberInfoRepository.java
+++ src/main/java/com/secondproject/project/repository/MemberInfoRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.secondproject.project.entity.MemberInfoEntity;
 
 public interface MemberInfoRepository extends JpaRepository<MemberInfoEntity, Long>{    
-    public Integer countByMiEmail(String miEamil);
+    public Integer countByMiEmail(String miEmail);
     public Integer countByMiNickname(String miNickname);
+    public MemberInfoEntity findByMiEmailAndMiPwd(String miEmail, String miPwd);
+    public MemberInfoEntity findByMiSeq(Long miSeq);
 }
