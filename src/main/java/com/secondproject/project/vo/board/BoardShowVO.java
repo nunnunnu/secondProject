378aad8941d578149ed58b073536215c0b998d34
jinhee <bis04199@gmail.com>
2023-02-20 16:56:47 +0900

    swagger

--- src/main/java/com/secondproject/project/vo/board/BoardShowVO.java
+++ src/main/java/com/secondproject/project/vo/board/BoardShowVO.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import com.secondproject.project.entity.BoardInfoEntity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,11 +14,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "게시글 조회")
 public class BoardShowVO {
+    @Schema(description = "번호")
     private Long seq;
+    @Schema(description = "글제목")
     private String title;
+    @Schema(description = "글 작성일")
     private LocalDate regDt;
+    @Schema(description = "댓글 수")
     private int comment;
+    @Schema(description = "좋아요 수")
     private Long likes;
 
     public BoardShowVO(BoardInfoEntity entity, Long likes){
